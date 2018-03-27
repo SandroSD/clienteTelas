73060fff3f5559c90833635254d35e8152fc17b1
Sandro Dezerio <sdezerio@gmail.com>
2018-03-27 16:06:27 -0300

    cambios

--- secciones/home.php
+++ secciones/home.php
@@ -5,7 +5,7 @@
             <!--span class="text-primary">SEGUPLAST</span-->
         </h1>
         </div>
-        <div class="col-md-9 mt-3">
+        <div class="col-md-9 col-sm-12 mt-3">
             <div id="contenedorCarousel">
                 <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
                     <div class="carousel-inner">
@@ -19,30 +19,30 @@
                         <img class="d-block w-100" src="img/home/img3.jpg" alt="Third slide">
                         </div>
                     </div>
-                    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                    <!--a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
                         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                         <span class="sr-only">Previous</span>
                     </a>
                     <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
                         <span class="carousel-control-next-icon" aria-hidden="true"></span>
                         <span class="sr-only">Next</span>
-                    </a>
+                    </a-->
                 </div>
             </div>
             <div id="contenedorMarcas">
-                <div class="col-md-12">
+                <div class="col-md-12 col-sm-12">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, odit neque? Esse, non dolore error repudiandae labore soluta facilis quod dolorem
                     <div class="row">
-                    <div class="col-md-3">
+                    <div class="col-md-3 col-sm-6">
                         <img src="img/home/img1.jpg" class="imagenHome">
                     </div>
-                    <div class="col-md-3">
+                    <div class="col-md-3 col-sm-6">
                         <img src="img/home/img1.jpg" class="imagenHome">
                     </div>
-                    <div class="col-md-3">
+                    <div class="col-md-3 col-sm-6">
                         <img src="img/home/img1.jpg" class="imagenHome">
                     </div>
-                    <div class="col-md-3">
+                    <div class="col-md-3 col-sm-6">
                         <img src="img/home/img1.jpg" class="imagenHome">
                     </div>  
                     </div>
@@ -50,7 +50,7 @@
                 </div>
             </div>
         </div>
-        <div class="col-sm-3 mt-3">
+        <div class="col-md-3 col-sm-12 mt-3">
         <div id="contenidoNoticias">
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit voluptas inventore magnam accusamus. Neque ipsam inventore facere aliquam laudantium ipsum sequi ab voluptates magni fugiat temporibus cumque officia, necessitatibus facilis.</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit voluptas inventore magnam accusamus. Neque ipsam inventore facere aliquam laudantium ipsum sequi ab voluptates magni fugiat temporibus cumque officia, necessitatibus facilis.</p>
