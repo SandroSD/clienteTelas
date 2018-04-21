4df910e877e9231c91d84ff7f60f36994ab555bb
SandroSD <sdezerio@gmail.com>
2018-04-21 18:00:54 -0300

    cambio

--- secciones/home.php
+++ secciones/home.php
@@ -20,7 +20,7 @@
             <div class="col-12 col-md-12 col-sm-12 mt-3">
                 
                 <div id="contenidoNoticias">
-                    Seguimos las últimas tendencias y nos renovamos permanentemente. Además de atenderte con una sonrisa, te ofrecemos descuentos por pago en efectivo y cuotas sin interés con todas las tarjetas de crédito.<br> ¡Atento a las promociones! <br><br> ¡Te invitamos a conocernos!
+                    Seguimos las últimas tendencias y nos renovamos permanentemente. Además de atenderte con una sonrisa, te ofrecemos descuentos por pago en efectivo y cuotas sin interés con todas las tarjetas de crédito.<br>¡Atento a las promociones!
                     <!--p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit voluptas inventore magnam accusamus. Neque ipsam inventore facere aliquam laudantium ipsum sequi ab voluptates magni fugiat temporibus cumque officia, necessitatibus facilis.</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit voluptas inventore magnam accusamus. Neque ipsam inventore facere aliquam laudantium ipsum sequi ab voluptates magni fugiat temporibus cumque officia, necessitatibus facilis.</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit voluptas inventore magnam accusamus. Neque ipsam inventore facere aliquam laudantium ipsum sequi ab voluptates magni fugiat temporibus cumque officia, necessitatibus facilis.</p>
@@ -29,6 +29,7 @@
                 </div>        
                 <div id="contenedorMarcas">
                     <div class="col-12 col-md-12 col-sm-12">
+                    <p align="center" style="color: black;">¡Te invitamos a conocernos!</p>                    
                         <hr class="style14">
                         <div class="row">
                             <div class="col-4 col-md-3 col-sm-6 col-lg-4">
