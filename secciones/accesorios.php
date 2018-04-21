670e146a6bfa275d1a65abbba9a1d8015224582a
SandroSD <sdezerio@gmail.com>
2018-04-21 16:44:41 -0300

    cambios

--- secciones/accesorios.php
+++ secciones/accesorios.php
@@ -5,19 +5,19 @@
             <div class="col-md-12 col-lg-12">
                 <div class="row">
                     
-                            <div class="col-md-6 col-lg-3">
+                            <div class="col-6 col-md-6 col-lg-3">
                                 <div class="accesoriosT">                                    
                                         <img class="imgAcc rounded-circle" src="img/nuestrosProductos/accesorios/cartones.jpg" alt="">
                                         <div class="txtAcc">Carton</div>                                                                                                                        
                                 </div>
                             </div>
-                            <div class="col-md-6 col-lg-3">
+                            <div class="col-6 col-md-6 col-lg-3">
                                 <div class="accesoriosT">                                    
                                     <img class="imgAcc rounded-circle" src="img/nuestrosProductos/accesorios/cemento.jpg" alt="">
                                     <div class="txtAcc">Cemento</div>
                                 </div>
                             </div>
-                            <div class="col-md-6 col-lg-3">
+                            <div class="col-6 col-md-6 col-lg-3">
                                 <div class="accesoriosT">                                    
                                     <img class="imgAcc rounded-circle" src="img/nuestrosProductos/accesorios/cierresDeslisadores.jpg" alt="">                                    
                                     <div class="txtAcc">Cierre</div>
@@ -25,19 +25,19 @@
                             </div>
                     
                     
-                            <div class="col-md-6 col-lg-3">
+                            <div class="col-6 col-md-6 col-lg-3">
                                 <div class="accesoriosT">                                    
                                     <img class="imgAcc rounded-circle" src="img/nuestrosProductos/accesorios/cinchaElastica.jpg" alt="">
                                     <div class="txtAcc">Cincha</div>
                                 </div>
                             </div>
-                            <div class="col-md-6 col-lg-3">
+                            <div class="col-6 col-md-6 col-lg-3">
                                 <div class="accesoriosT">
                                     <img class="imgAcc rounded-circle" src="img/nuestrosProductos/accesorios/deslizadores.jpg" alt="">
                                     <div class="txtAcc">Deslizadores</div>
                                 </div>
                             </div>
-                            <div class="col-md-6 col-lg-3">
+                            <div class="col-6 col-md-6 col-lg-3">
                                 <div class="accesoriosT">
                                     <img class="imgAcc rounded-circle" src="img/nuestrosProductos/accesorios/flejeDeAluminio.jpg" alt="">
                                     <div class="txtAcc">Fleje de Aluminio</div>
@@ -45,76 +45,76 @@
                             </div>
                     
                     
-                            <div class="col-md-6 col-lg-3">
+                            <div class="col-6 col-md-6 col-lg-3">
                                 <div class="accesoriosT">
                                     <img class="imgAcc rounded-circle" src="img/nuestrosProductos/accesorios/grampas.jpg" alt="">
                                     <div class="txtAcc">Grampas</div>
                                 </div>
                             </div>
-                            <div class="col-md-6 col-lg-3">
+                            <div class="col-6 col-md-6 col-lg-3">
                                 <div class="accesoriosT">
                                     <img class="imgAcc rounded-circle" src="img/nuestrosProductos/accesorios/guata.jpg" alt="">
                                     <div class="txtAcc">Guata</div>
                                 </div>
                             </div>
-                            <div class="col-md-6 col-lg-3">
+                            <div class="col-6 col-md-6 col-lg-3">
                                 <div class="accesoriosT">
                                     <img class="imgAcc rounded-circle" src="img/nuestrosProductos/accesorios/hilos.jpg" alt="">
                                     <div class="txtAcc">Hilos</div>
                                 </div>
                             </div>
                     
-                            <div class="col-md-6 col-lg-3">
+                            <div class="col-6 col-md-6 col-lg-3">
                                 <div class="accesoriosT">
                                     <img class="imgAcc rounded-circle" src="img/nuestrosProductos/accesorios/hormillas.jpg" alt="">
                                     <div class="txtAcc">Hormillas</div>
                                 </div>
                             </div>
-                            <div class="col-md-6 col-lg-3">
+                            <div class="col-6 col-md-6 col-lg-3">
                                 <div class="accesoriosT">
                                     <img class="imgAcc rounded-circle" src="img/nuestrosProductos/accesorios/semillas.jpg" alt="">
                                     <div class="txtAcc">Semillas</div>
                                 </div>
                             </div>
-                            <div class="col-md-6 col-lg-3">
+                            <div class="col-6 col-md-6 col-lg-3">
                                 <div class="accesoriosT">
                                     <img class="imgAcc rounded-circle" src="img/nuestrosProductos/accesorios/tachas.jpg" alt="">
                                     <div class="txtAcc">Tachas</div>
                                 </div>
                             </div>
                     
-                            <div class="col-md-6 col-lg-3">
+                            <div class="col-6 col-md-6 col-lg-3">
                                 <div class="accesoriosT">
                                     <img class="imgAcc rounded-circle" src="img/nuestrosProductos/accesorios/tachuela.jpg" alt="">
                                     <div class="txtAcc">Tachuelas</div>
                                 </div>
                             </div>
-                            <div class="col-md-6 col-lg-3">
+                            <div class="col-6 col-md-6 col-lg-3">
                                 <div class="accesoriosT">
                                     <img class="imgAcc rounded-circle" src="img/nuestrosProductos/accesorios/ojales.jpg" alt="">
                                     <div class="txtAcc">Ojales</div>
                                 </div>
                             </div>
-                            <div class="col-md-6 col-lg-3">
+                            <div class="col-6 col-md-6 col-lg-3">
                                 <div class="accesoriosT">
                                     <img class="imgAcc rounded-circle" src="img/nuestrosProductos/accesorios/resortes.jpg" alt="">
                                     <div class="txtAcc">Resortes</div>
                                 </div>
                             </div>
                     
-                            <div class="col-md-6 col-lg-3">
+                            <div class="col-6 col-md-6 col-lg-3">
                                 <div class="accesoriosT">
                                     <img class="imgAcc rounded-circle" src="img/nuestrosProductos/accesorios/vellon.jpg" alt="">
                                     <div class="txtAcc">Vellon</div>
                                 </div>
                             </div>
-                            <div class="col-md-6 col-lg-3">
+                            <div class="col-6 col-md-6 col-lg-3">
                                 <div class="accesoriosT">
                                     <img class="imgAcc rounded-circle" src="img/nuestrosProductos/accesorios/vivoPlastico.jpg" alt="">
                                     <div class="txtAcc">Vivo Plástico</div>
                                 </div>
                             </div>
-                            <div class="col-md-6 col-lg-3">
+                            <div class="col-6 col-md-6 col-lg-3">
                                 <div class="accesoriosT">
                                     <img class="imgAcc rounded-circle" src="img/nuestrosProductos/accesorios/vivoAlgodon.jpg" alt="">
                                     <div class="txtAcc">Vivo de Algodón</div>
