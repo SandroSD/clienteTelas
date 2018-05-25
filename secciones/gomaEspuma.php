d2d3db50977286d6f9bef9fca6675249b33324fc
SandroSD <sdezerio@gmail.com>
2018-05-25 01:25:40 -0300

    arreglamos GE

--- secciones/gomaEspuma.php
+++ secciones/gomaEspuma.php
@@ -3,28 +3,28 @@
     <br><hr class="style14"><br>
         <div class="row">
             <div class="col-md-12 col-lg-12 col-12">
-                <div class="tituloGE">
+                <!--div class="tituloGE">
                     <span id="contenidoTituloGE">
                         SERVICIO DE CORTE DE GOMA ESPUMA A MEDIDA
                     </span>                    
-                </div>                
+                </div-->                
                 <div id="fotoTituloGE">                    
                     <img src="../img/nuestrosProductos/gomaEspuma/gomaEspuma.jpg" id="fotoGE" alt="">    
                 </div>
-                <div class="tituloGE">
+                <!--div class="tituloGE">
                     <span id="contenidoPieGE">
                         
                     </span>                    
-                </div>
+                </div-->
                 <!--div>
                     <img class="rounded-circle" id="imagenGE" src="img/nuestrosProductos/gomaEspuma/ge1.jpg" />
                 </div-->
                 <div align="center">
                 <div  style="font-size:1.15em;">
-                    <span><i class="fas fa-check"></i> Contamos con goma espuma de diferentes medidas y calidades de media y alta densidad.</span><br>
+                    <span><i class="fas fa-check"></i> Contamos con goma espuma de diferentes medidas y calidades de media y alta densidad.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br>
                     <span><i class="fas fa-check"></i> Realizamos cortes a medida, por ejemplo de Almohadas, Almohadones, Catres, Colchonetas.</span><br>
-                    <span><i class="fas fa-check"></i> Confeccionamos rellenos para Sillas, Sillones, Cilindros, Cubos, y Tapicería en general.</span><br>
-                    <span><i class="fas fa-check"></i> Fabricamos colchones de alta densidad, combinando diferentes materiales (sin resortes).</span>
+                    <span><i class="fas fa-check"></i> Confeccionamos rellenos para Sillas, Sillones, Cilindros, Cubos, y Tapicería en general.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><br>
+                    <span><i class="fas fa-check"></i> Fabricamos colchones de alta densidad, combinando diferentes materiales (sin resortes).&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
                 </div>
                 </div>
                 
